c1d82ea2ecb3905313d3e784059bcaeb91d40efb
Matthew Blissett <mblissett@gbif.org>
2017-04-24 14:04:42 +0200

    Initial commit.
    Fork of occurrence density version, with new stylesheets.

---
+++ src/main/node/cartoParser.js
@@ -0,0 +1,64 @@
+var mapnik = require('mapnik')
+  , tm = require('./tm')
+  , _ = require('underscore')
+  , carto = require('carto');
+
+var parser = {}
+
+/**
+ * Method originally taken from mapbox and then stripped of much logic to reduce to a simple parser.
+ * https://github.com/mapbox/mapbox-studio-classic/blob/mb-pages/lib/style.js#L192
+ *
+ * Requires:
+ * - styles: An array of Strings each containing the CartoCSS
+ * - tilejson: A String of tilejson metadata
+ *
+ * Returns mapnik stylesheet XML.
+ */
+
+parser.parseToXML = function(styles, tilejson) {
+  // Include params to be written to XML.
+  var opts = [
+    'name',
+    'description',
+    'attribution',
+    'bounds',
+    'center',
+    'format',
+    'minzoom',
+    'maxzoom',
+    'source',
+    'template'
+  ]
+  opts.srs = tm.srs['900913'];
+
+  // Convert datatiles sources to mml layers.
+  var layers = tilejson.data.vector_layers;
+
+  opts.Layer = layers.map(function(layer) {
+    return {
+      id:layer.id,
+      name:layer.id,
+      'class':layer['class'],
+      srs:tm.srs['900913']
+    }
+  });
+
+  opts.Stylesheet = _(styles).map(function(style,basename) {
+    return {
+      id: basename,
+      data: style
+    };
+  });
+
+  try {
+    return new carto.Renderer(null, {
+      mapnik_version: mapnik.versions.mapnik
+    }).render(opts);
+
+  } catch(err) {
+    throw ("Error parsing CartoCSS: " + err.message);
+  }
+}
+
+module.exports = parser;
---
+++ src/main/node/server.js
@@ -0,0 +1,302 @@
+var mapnik = require('mapnik'),
+    mercator = require('./sphericalmercator'),
+    request = require('request'),
+    http = require('http'),
+    url = require('url'),
+    fs = require('fs'),
+    yaml = require('yaml-js'),
+    gbifServiceRegistry = require('./gbifServiceRegistry'),
+    parser = require('./cartoParser');
+
+/**
+ * Compile the CartoCss into Mapnik stylesheets into a lookup dictionary
+ */
+var namedStyles = {};
+namedStyles["osm-bright"] = compileStylesheetSync(['mapbox-studio-osm-bright.tm2/style.mss', 'mapbox-studio-osm-bright.tm2/road.mss', 'mapbox-studio-osm-bright.tm2/labels.mss']);
+namedStyles["gbif-classic"] = compileStylesheetSync(["./cartocss/gbif-classic.mss"]);
+namedStyles["gbif-dark"] = compileStylesheetSync(["./cartocss/gbif-dark.mss"]);
+namedStyles["gbif-middle"] = compileStylesheetSync(["./cartocss/gbif-middle.mss"]);
+namedStyles["gbif-light"] = compileStylesheetSync(["./cartocss/gbif-light.mss"]);
+function compileStylesheetSync(filename) {
+  // snippet simulating a tilejson response from tilelive, required only to give the layers for the cartoParser
+  var tilejson = {
+    data: {
+      "vector_layers": [
+	      // This defines the order of the layers, so road labels appear over roads, for example.
+	      {"id": "landuse" },
+	      {"id": "landcover" },
+	      {"id": "water" },
+	      {"id": "waterway" },
+	      {"id": "park" },
+	      {"id": "contour" },
+	      {"id": "boundary" },
+	      {"id": "transportation" },
+	      {"id": "aeroway" },
+	      {"id": "building" },
+	      {"id": "water_name" },
+	      {"id": "place" },
+	      {"id": "poi" },
+	      {"id": "transportation_name" },
+	      {"id": "housenumber" }
+      ]
+    }
+  };
+  var cartocss = filename.map( (f) => fs.readFileSync(f, "utf8") );
+  return parser.parseToXML(cartocss, tilejson);
+}
+var defaultStyle = "classic.point";
+
+// Fonts
+// Register default fonts.
+//mapnik.register_fonts(path.dirname(require.resolve('mapbox-studio-pro-fonts')), { recurse: true });
+//apnik.register_fonts(path.dirname(require.resolve('mapbox-studio-default-fonts')), { recurse: true });
+mapnik.register_fonts('/usr/share/fonts/truetype/', { recurse: true });
+mapnik.register_fonts('/usr/share/fonts/opentype/', { recurse: true });
+mapnik.register_fonts('./mapbox-studio-default-fonts/open-sans', { recurse: true });
+//mapnik.register_default_fonts();
+//mapnik.register_system_fonts();
+mapnik.register_default_input_plugins();
+
+//mapnik.register_fonts((require.resolve('./mapbox-studio-default-fonts')), { recurse: true });
+//console.log("Fonts", mapnik.fonts());
+//console.log("Fonts", mapnik.face_names());
+
+var processStartTime = new Date().toUTCString();
+console.log("HTTP requests will have Last-Modified set to", processStartTime);
+
+/**
+ * The server supports the ability to provide assets which need to be explicitly registered in order to be secure.
+ * (e.g. trying to expose files using URL hack such as http://api.gbif.org/v1/map/../../../hosts)
+ *
+ * Should this become more complex, then express or similar should be consider.
+ */
+var assetsHTML = [
+  '/3857.html',
+  '/4326.html'
+]
+
+function parseUrl(parsedRequest) {
+  if (parsedRequest.pathname.endsWith(".png")) {
+
+	  // extract the x,y,z from the URL which could be /4326/omt/{z}/{x}/{y}@{n}x.png
+	  var dirs = parsedRequest.pathname.substring(0, parsedRequest.pathname.length - 7).split("/");
+	  var z = parseInt(dirs[dirs.length - 3]);
+	  var x = parseInt(dirs[dirs.length - 2]);
+	  var y = parseInt(dirs[dirs.length - 1]);
+
+	  var xOut = x;
+	  var yOut = y;
+	  var zOut = z;
+	  var xOffset = 0;
+	  var yOffset = 0;
+	  if (z == 16) {
+	    zOut = 14;
+	    xOut = parseInt(x/4);
+	    yOut = parseInt(y/4);
+	    xOffset = x%4;
+	    yOffset = y%4;
+	    parsedRequest.pathname = parsedRequest.pathname.replace(z+'/'+x+'/'+y, zOut+'/'+xOut+'/'+yOut);
+	    console.log("High zoom", z, x, y, '->', zOut, xOut+'+'+xOffset, yOut+'+'+yOffset, parsedRequest.pathname);
+	  } else if (z == 15) {
+	    zOut = 14;
+	    xOut = parseInt(x/2);
+	    yOut = parseInt(y/2);
+	    xOffset = x%2;
+	    yOffset = y%2;
+	    parsedRequest.pathname = parsedRequest.pathname.replace(z+'/'+x+'/'+y, zOut+'/'+xOut+'/'+yOut);
+	    console.log("High zoom", z, x, y, '->', zOut, xOut+'+'+xOffset, yOut+'+'+yOffset, parsedRequest.pathname);
+	  }
+
+	  // find the compiled stylesheet from the given style parameter, defaulting if omitted or bogus
+	  var stylesheet = (parsedRequest.query.style in namedStyles)
+	      ? namedStyles[parsedRequest.query.style]
+	      : namedStyles[defaultStyle];
+
+	  var density = parseInt(parsedRequest.pathname.substring(parsedRequest.pathname.length - 6, parsedRequest.pathname.length - 5));
+
+	  if (!(isNaN(z) || isNaN(x) || isNaN(y) || isNaN(density))) {
+	    return {
+		    "z": z,
+		    "zOut": zOut,
+		    "x": x,
+		    "xOffset": xOffset,
+		    "y": y,
+		    "yOffset": yOffset,
+		    "density": density,
+		    "stylesheet": stylesheet
+	    }
+	  }
+  }
+  throw Error("URL structure is invalid, expected /SRS/tileset/{z}/{x}/{y}@{n}x.png");
+}
+
+function vectorRequest(parsedRequest) {
+  // reformat the request to the type expected by the VectorTile Server
+  parsedRequest.search = undefined;
+  parsedRequest.query = undefined;
+  parsedRequest.pathname = parsedRequest.pathname.replace("@1x.png", ".pbf");
+  parsedRequest.pathname = parsedRequest.pathname.replace("@2x.png", ".pbf");
+  parsedRequest.pathname = parsedRequest.pathname.replace("@3x.png", ".pbf");
+  parsedRequest.pathname = parsedRequest.pathname.replace("@4x.png", ".pbf");
+  parsedRequest.hostname = config.tileServer.host;
+  parsedRequest.port = config.tileServer.port;
+  parsedRequest.protocol = "http:";
+  return url.format(parsedRequest);
+}
+
+function createServer(config) {
+  return http.createServer(function(req, res) {
+    console.log("Request: "+req.url);
+
+    var parsedRequest = url.parse(req.url, true)
+
+    // handle registered assets
+	  console.log(parsedRequest.pathname);
+    if (assetsHTML.indexOf(parsedRequest.pathname) != -1) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(fs.readFileSync('./public' + parsedRequest.pathname));
+    } else {
+
+	    // Handle map tiles.
+	    try {
+	      var parameters = parseUrl(parsedRequest);
+	    } catch (e) {
+	      res.writeHead(400, { 'Content-Type': 'text/plain' });
+	      res.end(e.message);
+	      return;
+	    }
+
+	    var vectorTileUrl = vectorRequest(parsedRequest);
+
+
+	    // issue the request to the vector tile server and render the tile as a PNG using Mapnik
+	    console.time("getTile");
+	    console.log("Fetching vector tile", vectorTileUrl);
+	    request.get({url: vectorTileUrl, method: 'GET', encoding: null}, function (error, response, body) {
+
+	      console.log("Vector tile has HTTP status", response.statusCode, "and size", body.length);
+
+        if (!error && response.statusCode == 200 && body.length > 0) {
+          console.timeEnd("getTile");
+
+          var size = 512 * parameters.density;
+
+	        try {
+		        var map = new mapnik.Map(size, size, mercator.proj4);
+		        map.fromStringSync(parameters.stylesheet);
+		        // Pretend it's tile 0, 0, since Mapnik validates the address according to the standard Google schema,
+		        // and we aren't using it for WGS84.
+		        var vt = new mapnik.VectorTile(parameters.zOut, 0, 0);
+		        vt.addDataSync(body);
+
+		        var options = {"buffer_size": 128, "scale": parameters.density};
+		        if (parameters.z > 14) {
+		          options.z = parameters.z;
+		          options.x = parameters.xOffset;
+		          options.y = parameters.yOffset;
+		        }
+
+		        // important to include a buffer, to catch the overlaps
+		        //console.time("render");
+		        vt.render(map, new mapnik.Image(size, size), options, function (err, image) {
+		          if (err) {
+			          res.end(err.message);
+		          } else {
+			          // if response.headers.last-modified is set, and is more recent than processStartTime, use that instead.
+			          res.writeHead(200, {
+			            'Content-Type': 'image/png',
+			            'Access-Control-Allow-Origin': '*',
+			            'Cache-Control': 'public, max-age=604800', // 1 week
+			            'Last-Modified': processStartTime
+			          });
+			          //console.timeEnd("render");
+
+			          image.encode('png', function (err, buffer) {
+			            if (err) {
+				            res.end(err.message);
+			            } else {
+				            res.end(buffer);
+			            }
+			          });
+		          }
+		        });
+	        } catch (e) {
+		        // something went wrong
+		        res.writeHead(500, {'Content-Type': 'image/png'}); // type only for ease of use with e.g. leaflet
+		        res.end(e.message);
+		        console.log(e);
+	        }
+
+        } else if (!error && (
+          response.statusCode == 404 ||   // not found
+          response.statusCode == 204 ||   // no content
+          (response.statusCode == 200 && body.length==0))  // accepted but no content
+		              ) {
+          // no tile
+          res.writeHead((response.statusCode == 200) ? 204 : response.statusCode, // keep same status code
+                        {
+			                    'Content-Type': 'image/png',
+			                    'Access-Control-Allow-Origin': '*',
+			                    'Cache-Control': 'public, max-age=604800', // 1 week
+			                    'Last-Modified': processStartTime
+                        });
+          res.end();
+
+        } else {
+          // something went wrong
+          res.writeHead(503, {'Content-Type': 'image/png'}); // type only for ease of use with e.g. leaflet
+          res.end();
+        }
+	    })
+    }
+  });
+}
+
+/**
+ * Shut down cleanly.
+ */
+function exitHandler() {
+  console.log("Completing requests");
+  // Until https://github.com/nodejs/node/issues/2642 is fixed, we can't wait for connections to end.
+  //server.close(function () {
+    process.exit(0);
+  //});
+}
+
+/**
+ * The main entry point.
+ * Extract the configuration and start the server.  This expects a config file in YAML format and a port
+ * as the only arguments.  No sanitization is performed on the file existance or content.
+ */
+try {
+  process.on('SIGHUP', () => {console.log("Ignoring SIGHUP")});
+
+  // Log if we crash.
+  process.on('uncaughtException', function (exception) {
+    console.trace(exception);
+    exitHandler();
+  });
+  process.on('unhandledRejection', (reason, p) => {
+    console.log("Unhandled Rejection at: Promise ", p, " reason: ", reason);
+    exitHandler();
+  });
+
+  // Set up server.
+  var configFile = process.argv[2];
+  var port = parseInt(process.argv[3]);
+  console.log("Using config: " + configFile);
+  console.log("Using port: " + port);
+  var config = yaml.load(fs.readFileSync(configFile, "utf8"));
+  server = createServer(config)
+  server.listen(port);
+
+  // Set up ZooKeeper.
+  gbifServiceRegistry.register(config);
+
+  // Aim to exit cleanly.
+  process.on('SIGINT', exitHandler.bind());
+  process.on('SIGTERM', exitHandler.bind());
+  process.on('exit', exitHandler.bind());
+} catch (e) {
+  console.error(e);
+}
